16bfe4e079318f5a773c433e04a814c903028bbe
alexrod94 <alexrod94@gmail.com>
2024-08-10 10:18:56 +0200

    Add test endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-router.get("/", isAuthenticated, (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
